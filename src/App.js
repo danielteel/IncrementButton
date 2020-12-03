55ad2e763b42f235e4d4f5e458a736be3fce2376
Daniel Teel <daniel.teel.2@us.af.mil>
2020-12-03 15:10:46 -0700

    changed button to generic component for reuseability

--- src/App.js
+++ src/App.js
@@ -1,66 +1,54 @@
 import React from 'react';
-import IncrementButton from './IncrementButton';
+import GenericButton from './GenericButton';
 import './App.css';
 
+
+
 export default class App extends React.Component {
- constructor(props){
-    super()
-    this.state = {count: 0, buttons:[]};
- }
- handleSubmit = (event) => {
-     event.preventDefault()
-    //generate button
-    let newButtons=this.state.buttons;
-    let newAmount=Number(event.target.increment.value);
-    if (this.state.buttons.includes(newAmount)){
-        return;
+    constructor(props){
+        super(props)
+        this.state = {count: 0, buttons:[]};
     }
-    newButtons.push(newAmount);
-    this.setState({ buttons: newButtons});
- }
 
- incrementCounter = (amount) => {
-    this.setState({ count: this.state.count + amount});
- }
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        let newAmount=Number(event.target.addThis.value);
+        if (this.state.buttons.includes(newAmount)) return;
+
+        this.setState({ buttons: [...this.state.buttons, newAmount]});
+    }
+
+    incrementCounter = (amount) => {
+        this.setState( { count: this.state.count + amount} );
+    }
+
+    resetButtons = () => {
+        this.setState( {buttons: [] } );
+    }
+    resetCounter = () => {
+        this.setState({count: 0})
+    }
  
- render (){
-     return (
-         <div className="app">
-             <title> test </title>
-             <header>
-                 <h1>Custom Counter</h1>
-                 <p>{this.state.count}</p>
-            </header> 
-            <form onSubmit={ this.handleSubmit }>
-                <div>
-                    <input type="number" name="increment"/>
-                    <button type="submit">generate button</button>
+    render (){
+        return (
+                <div className="app">
+                    <header>
+                        <h1>Custom Counter</h1>
+                        <p>{this.state.count}</p>
+                    </header> 
+                    <form onSubmit={ this.handleSubmit }>
+                        <div>
+                            <input type="number" name="addThis"/>
+                            <button type="submit">Generate Button</button>
+                        </div>
+                            <div className="new-buttons">
+                                {this.state.buttons.map( val => <GenericButton onClick={this.incrementCounter} valueToPass={val}>{"Increment by "+val}</GenericButton> ) }
+                            </div>
+                    </form>
+                    <GenericButton onClick={this.resetButtons}> Reset all the buttons! </GenericButton>
+                    <GenericButton onClick={this.resetCounter}> Reset counter </GenericButton>
                 </div>
-                <div className="new-buttons">
-                {
-                 this.state.buttons.map( (buttonVal) => {
-                    return <IncrementButton onClick={this.incrementCounter} amountToIncrement={buttonVal}/>
-                 })
-                }
-                </div>
-            </form>
-
-         </div>
-
-     )
- }
+            );
+    }
 }
-
-//           <div>{ this.state.count }</div>
-//           <form onSubmit={this.handleSubmit}>
-//             <input type="number" name="increment" />
-//             <button type="submit">generate button</button>
-//           </form>
-//           <button>add 4</button>
-//           <button>add 8</button>
-//         </div>
-//       );
-//     }
-//   }
-
-
